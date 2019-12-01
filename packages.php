fe29d50b645527e88a2290380f9d89d2ec6d02fe
Muhammad Adil <adilmemon887@gmail.com>
2019-12-01 15:04:34 +0500

    Add packages.php

---
+++ packages.php
@@ -0,0 +1,225 @@
+<?php
+include("header.php");
+
+?>
+
+<!-- start banner Area -->
+<section class="about-banner relative">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Tour Packages				
+							</h1>	
+							<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="packages.html"> Tour Packages</a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start hot-deal Area -->
+			<section class="hot-deal-area section-gap">
+				<div class="container">
+		            <div class="row d-flex justify-content-center">
+		                <div class="menu-content pb-70 col-lg-8">
+		                    <div class="title text-center">
+		                        <h1 class="mb-10">Today’s Hot Deals</h1>
+		                        <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast, day to.</p>
+		                    </div>
+		                </div>
+		            </div>						
+					<div class="row justify-content-center">
+						<div class="col-lg-10 active-hot-deal-carusel">
+							<div class="single-carusel">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>
+									<img class="img-fluid" src="usertemplate/img/packages/hot-deal.jpg" alt="">
+								</div>
+								<div class="price-detials">
+									<a href="#" class="price-btn">Starting From <span>$250</span></a>
+								</div>
+								<div class="details">
+									<h4 class="text-white">Ancient Architecture</h4>
+									<p class="text-white">
+										Cairo, Egypt
+									</p>
+								</div>								
+							</div>
+							<div class="single-carusel">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>
+									<img class="img-fluid" src="usertemplate/img/packages/hot-deal.jpg" alt="">
+								</div>
+								<div class="price-detials">
+									<a href="#" class="price-btn">Starting From <span>$250</span></a>
+								</div>
+								<div class="details">
+									<h4 class="text-white">Ancient Architecture</h4>
+									<p class="text-white">
+										Cairo, Egypt
+									</p>
+								</div>								
+							</div>
+							<div class="single-carusel">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>
+									<img class="img-fluid" src="usertemplate/img/packages/hot-deal.jpg" alt="">
+								</div>
+								<div class="price-detials">
+									<a href="#" class="price-btn">Starting From <span>$250</span></a>
+								</div>
+								<div class="details">
+									<h4 class="text-white">Ancient Architecture</h4>
+									<p class="text-white">
+										Cairo, Egypt
+									</p>
+								</div>								
+							</div>														
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End hot-deal Area -->
+			
+
+			<!-- Start destinations Area -->
+			<section class="destinations-area section-gap">
+				<div class="container">
+		            <div class="row d-flex justify-content-center">
+		                <div class="menu-content pb-40 col-lg-8">
+		                    <div class="title text-center">
+		                        <h1 class="mb-10">Popular Destinations</h1>
+		                        <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast, day to.</p>
+		                    </div>
+		                </div>
+		            </div>	
+                    <div class="row">					
+<?php
+include("connection.php");
+$query = "select * from packages";
+$r = mysqli_query($con,$query);
+
+
+
+    while ($row = mysqli_fetch_array($r)) {
+                    
+
+                   echo "<div class='col-lg-4'>";
+                   echo	"<div class='single-destinations'>";
+							echo "<div class='thumb'>";
+									echo "<img src='admin/uploads/".$row['img']."' alt=''>";
+                              echo "</div>";
+                              echo	"<div class='details'>";
+									echo "<h4>".$row['heading']."</h4>";
+									echo "<p>";
+                                    echo "".$row['description']."";
+                                        echo "</p>";
+                                        echo "<ul class='package-list'>";
+										echo "<li class='d-flex justify-content-between align-items-center'>";
+                                        echo "<span>".$row['column1']."</span>";
+                                        echo "<span>".$row['column1_value']."</span>";
+										echo "</li>";
+										echo "<li class='d-flex justify-content-between align-items-center'>";
+                                        echo "<span>".$row['column2']."</span>";
+                                        echo "<span>".$row['column2_value']."</span>";
+										echo "</li>";
+										echo "<li class='d-flex justify-content-between align-items-center'>";
+                                        echo "<span>".$row['column3']."</span>";
+										echo	"<span>".$row['column3_value']."</span>";
+										echo "</li>";
+										echo "<li class='d-flex justify-content-between align-items-center'>";
+                                        echo	"<span>".$row['column4']."</span>";
+										echo	"<span>".$row['column4_value']."</span>";
+										echo "</li>";
+										echo "<li class='d-flex justify-content-between align-items-center'>";
+                                        echo "<span>".$row['price']."</span>";
+										echo	"<a href='#' class='price-btn'>".$row['price_value']."</a>";
+										echo "</li>";					
+                                        echo "</ul>";
+                                        echo "</div>";
+                                        echo "</div>";
+                                        echo "</div>";
+						
+                     
+                    
+    }
+						?>
+                        
+						</div>																														
+					</div>
+				</div>	
+			</section>
+			<!-- End destinations Area -->
+			
+
+			<!-- Start home-about Area -->
+			<section class="home-about-area">
+				<div class="container-fluid">
+					<div class="row align-items-center justify-content-end">
+						<div class="col-lg-6 col-md-12 home-about-left">
+							<h1>
+								Did not find your Package? <br>
+								Feel free to ask us. <br>
+								We‘ll make it for you
+							</h1>
+							<p>
+								inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed.
+							</p>
+							<a href="#" class="primary-btn text-uppercase">request custom price</a>
+						</div>
+						<div class="col-lg-6 col-md-12 home-about-right no-padding">
+							<img class="img-fluid" src="usertemplate/img/packages/about-img.jpg" alt="">
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End home-about Area -->
+
+
+<?php
+include("footer.php");
+
+?>
+
+
+
+
+<!-- Working DATA
+<div class="row">
+						<div class="col-lg-4">
+							<div class="single-destinations">
+								<div class="thumb">
+									<img src="usertemplate/img/packages/d1.jpg" alt="">
+								</div>
+								<div class="details">
+									<h4>Holiday Sea beach Blue Ocean</h4>
+									<p>
+										United staes of America
+									</p>
+									<ul class="package-list">
+										<li class="d-flex justify-content-between align-items-center">
+											<span>Duration</span>
+											<span>06 days and 7 nights</span>
+										</li>
+										<li class="d-flex justify-content-between align-items-center">
+											<span>Date</span>
+											<span>18.04.2018</span>
+										</li>
+										<li class="d-flex justify-content-between align-items-center">
+											<span>Airport</span>
+											<span>Changi</span>
+										</li>
+										<li class="d-flex justify-content-between align-items-center">
+											<span>Extras</span>
+											<span>All Inclusive</span>
+										</li>
+										<li class="d-flex justify-content-between align-items-center">
+											<span>Price per person</span>
+											<a href="#" class="price-btn">$250</a>
+										</li>													
+									</ul>
+								</div>
+							</div>
+						</div> -->
